947f50ae278d8425a8a38569f2c1bfb705625e08
hanguk0726 <hanguk0726@gmail.com>
2023-02-24 13:19:30 +0900

    fix::yuyv

--- rust/src/capture.rs
+++ rust/src/capture.rs
@@ -5,11 +5,12 @@ use nokhwa::utils::{
     mjpeg_to_rgb, yuyv422_to_rgb, CameraFormat, CameraIndex, FrameFormat, RequestedFormat,
     RequestedFormatType,
 };
-use nokhwa::CallbackCamera;
+use nokhwa::{CallbackCamera, NokhwaError};
 use nokhwa_core::buffer::Buffer;
 use once_cell::sync::Lazy;
 use std::fmt::Error;
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 pub fn inflate_camera_conection(sender: Arc<Sender<Vec<u8>>>) -> Result<CallbackCamera, Error> {
     let index = CameraIndex::Index(0);
@@ -17,10 +18,13 @@ pub fn inflate_camera_conection(sender: Arc<Sender<Vec<u8>>>) -> Result<Callback
         RequestedFormat::new::<RgbAFormat>(RequestedFormatType::AbsoluteHighestFrameRate);
     let camera = CallbackCamera::new(index, requested, move |buf| {
         debug!("sending frame");
+        let start = Instant::now();
+        let buf = decode(buf).unwrap();
+        let duration = start.elapsed();
 
-        sender
-            .send(decode(buf).unwrap())
-            .expect("Error sending frame!");
+        debug!("Time elapsed in decode_function() is: {:?}", duration);
+
+        sender.send(buf).expect("Error sending frame!");
     })
     .map_err(|why| {
         eprintln!("Error opening camera: {:?}", why);
@@ -44,10 +48,11 @@ fn decode(buf: Buffer) -> Result<Vec<u8>, Error> {
             error!("Error converting MJPEG to RGB: {:?}", why);
             Error
         }),
-        FrameFormat::YUYV => yuyv422_to_rgb(data, true).map_err(|why| {
-            error!("Error converting YUYV to RGB: {:?}", why);
-            Error
-        }),
+        FrameFormat::YUYV => Ok(yuyv422_to_rgba_(data)),
+        // FrameFormat::YUYV => yuyv422_to_rgb(data, true).map_err(|why| {
+        //     error!("Error converting YUYV to RGB: {:?}", why);
+        //     Error
+        // }),
         // FrameFormat::GRAY => Ok(data
         //     .iter()
         //     .flat_map(|x| {
@@ -64,3 +69,31 @@ fn decode(buf: Buffer) -> Result<Vec<u8>, Error> {
         }
     }
 }
+
+fn yuyv422_to_rgba_(data: &[u8]) -> Vec<u8> {
+    let mut rgba = Vec::with_capacity(data.len() * 2);
+    for chunk in data.chunks_exact(4) {
+        let y0 = chunk[0] as f32;
+        let u = chunk[1] as f32;
+        let y1 = chunk[2] as f32;
+        let v = chunk[3] as f32;
+
+        let r0 = y0 + 1.370705 * (v - 128.);
+        let g0 = y0 - 0.698001 * (v - 128.) - 0.337633 * (u - 128.);
+        let b0 = y0 + 1.732446 * (u - 128.);
+
+        let r1 = y1 + 1.370705 * (v - 128.);
+        let g1 = y1 - 0.698001 * (v - 128.) - 0.337633 * (u - 128.);
+        let b1 = y1 + 1.732446 * (u - 128.);
+
+        rgba.extend_from_slice(&[
+            r0 as u8, g0 as u8, b0 as u8, 255, r1 as u8, g1 as u8, b1 as u8, 255,
+        ]);
+    }
+    rgba
+}
+
+// fn yuyv422_to_rgb_(data: &[u8], rgba: bool) ->  Result<Vec<u8>, NokhwaError> {
+    
+
+// }
