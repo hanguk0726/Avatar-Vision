7cd9da60624e8fa3c72790cec52742298cbcef9b
hanguk0726 <hanguk0726@gmail.com>
2023-03-11 16:30:22 +0900

    _

--- rust/src/channel_textrue.rs
+++ rust/src/channel_textrue.rs
@@ -10,7 +10,7 @@ use irondash_message_channel::{
 };
 use irondash_run_loop::RunLoop;
 use irondash_texture::{PixelDataProvider, SendableTexture};
-use kanal::{Receiver, Sender, AsyncReceiver, AsyncSender};
+use kanal::{AsyncReceiver, AsyncSender, Receiver, Sender};
 use log::debug;
 use nokhwa::Buffer;
 
@@ -61,46 +61,54 @@ impl AsyncMethodHandler for TextureHandler {
                 let started = std::time::Instant::now();
                 let mut count = 0;
 
-                let decode = |buf: Buffer, encoding_sender: Arc<AsyncSender<Vec<u8>>>,
-                                pixel_buffer: Arc<Mutex<Vec<u8>>>,
-                                texture_provider: Arc<SendableTexture<Box<dyn PixelDataProvider>>>
-                | {
+                let decode = |buf: Buffer,
+                              encoding_sender: Arc<AsyncSender<Vec<u8>>>,
+                              pixel_buffer: Arc<Mutex<Vec<u8>>>,
+                              texture_provider: Arc<
+                    SendableTexture<Box<dyn PixelDataProvider>>,
+                >| {
                     let time = std::time::Instant::now();
                     let mut decoded =
-                        decode_to_rgb(buf.buffer(), &buf.source_frame_format(), true).unwrap();
+                    decode_to_rgb(buf.buffer(), &buf.source_frame_format(), true).unwrap();
                     debug!(
                         "decoded frame, time elapsed: {}",
                         time.elapsed().as_millis()
                     );
-                    encoding_sender.as_ref().try_send(decoded.clone()).unwrap_or_else(|e| {
-                        debug!("encoding_sender.send failed: {:?}", e);
-                        false
-                    });
+                    encoding_sender
+                        .as_ref()
+                        .try_send(decoded.clone())
+                        .unwrap_or_else(|e| {
+                            debug!("encoding_sender.send failed: {:?}", e);
+                            false
+                        });
                     let mut pixel_buffer = pixel_buffer.lock().unwrap();
-                    
+
                     *pixel_buffer = take(&mut decoded);
                     debug!(
                         "mark_frame_available, pixel_buffer: {:?}",
                         pixel_buffer.len()
                     );
                     texture_provider.mark_frame_available();
-                    
                 };
                 let pool = tokio::runtime::Builder::new_multi_thread()
-                .worker_threads(4)
-                .build()
-                .unwrap();
-            
-            while let Ok(buf) = self.receiver.recv().await {
-                debug!("received buffer on texture channel");
-                let encoding_sender = Arc::clone( &self.encoding_sender);
-                let pixel_buffer = Arc::clone(&self.pixel_buffer);
-                let texture_provider = Arc::clone(&self.texture_provider);
+                    .worker_threads(8)
+                    .build()
+                    .unwrap();
+
+                while let Ok(buf) = self.receiver.recv().await {
+                    let time = std::time::Instant::now();
+                    let encoding_sender = Arc::clone(&self.encoding_sender);
+                    let pixel_buffer = Arc::clone(&self.pixel_buffer);
+                    let texture_provider = Arc::clone(&self.texture_provider);
+                    debug!("received buffer on texture channel");
                     pool.spawn(async move {
-                        
-                        decode(buf, encoding_sender , pixel_buffer, texture_provider);
+                        decode(buf, encoding_sender, pixel_buffer, texture_provider);
                     });
                     count += 1;
+                    debug!(
+                        "render_texture, time elapsed: {}",
+                        time.elapsed().as_millis()
+                    );
                 }
 
                 debug!(
